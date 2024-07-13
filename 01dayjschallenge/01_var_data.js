5c4abfa6bdc66ddcbf2c33d2f4265f598f854c11
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-07-13 09:30:50 +0000

    DAY1 COMPLETED

---
+++ 01dayjschallenge/01_var_data.js
@@ -0,0 +1,66 @@
+
+// Activity 1
+// Task1
+
+var num = 1082002;
+console.log(num);
+
+// Task2
+
+let name = "Challenge Accepted";
+console.log(name);
+
+// Activity 2
+// Task 3
+
+const bollean = true;
+console.log(bollean);
+
+// Activity 3
+// Task 4
+
+let number = 12344;
+console.log(typeof(number));
+
+let string = "Task done";
+console.log(typeof (string));
+
+let boolean = false;
+console.log(typeof (boolean));
+
+let object = {
+    name: "Sagar",
+    email: "sagar@gmail.com",
+    phoneNumber: "0987654321",
+};
+console.log(typeof (object));
+
+let array = [
+    "javaScript" , "python", "cpp"
+]
+
+console.log(typeof (array));
+
+
+
+// Activity 4
+// Task 5
+
+
+let userName = "Sagar";
+userName = "Ram";
+console.log(userName);
+console.log(userName);
+
+
+
+// Activity 5
+// Task 6
+
+const isLoggedin = true;
+console.log(isLoggedin);
+
+isLoggedin = false;
+console.log(isLoggedin);
+
+// -------------------------------------------------------------------------- DAY 1 COMPLETED -----------------------
